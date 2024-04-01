c80979e5d82b98004145fed53af76a30978fb676
VannyVet <vannygb07@gmail.com>
2024-04-01 14:49:58 +0700

    validation token

--- routes/api/v1/authApi.php
+++ routes/api/v1/authApi.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('user.register');
     Route::post('/login', [AuthController::class, 'login'])->name('user.login');
+    Route::post('/validate-token', [AuthController::class, 'validateToken'])->name('validation.token');
 
     Route::middleware('auth:api')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout'])->name('user.logout');
